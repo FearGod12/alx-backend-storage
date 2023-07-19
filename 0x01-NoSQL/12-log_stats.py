0927f7352755656eb06b7ce05c8dbb7874060996
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-19 05:25:23 -0700

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 """provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
 
+
+client = MongoClient()
+db = client.logs
+
+logs = db.nginx.count_documents({})
+get_count = db.nginx.count_documents({'method': 'GET'})
+post_count = db.nginx.count_documents({'method': 'POST'})
+put_count = db.nginx.count_documents({'method': 'PUT'})
+patch_count = db.nginx.count_documents({'method': 'PATCH'})
+delete_count = db.nginx.count_documents({'method': 'DELETE'})
+
+print("{} logs".format(logs))
+print("methods:")
+allcounts = {"GET": get_count, "POST": post_count, "PUT": put_count,
+             "PATCH": patch_count, "DELETE": delete_count}
+
+for key, value in allcounts.items():
+    print("\tmethod {}: {}".format(key, value))
+
+status = db.nginx.count_documents({'method': 'GET', 'path': '/status'})
+print("{} status check".format(status))
