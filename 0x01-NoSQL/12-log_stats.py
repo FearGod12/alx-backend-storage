9d57653846c1de5ce21ff1c9d02807196c72d9ed
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-19 05:31:56 -0700

    if __main__

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,23 +3,24 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
 
-logs = db.nginx.count_documents({})
-get_count = db.nginx.count_documents({'method': 'GET'})
-post_count = db.nginx.count_documents({'method': 'POST'})
-put_count = db.nginx.count_documents({'method': 'PUT'})
-patch_count = db.nginx.count_documents({'method': 'PATCH'})
-delete_count = db.nginx.count_documents({'method': 'DELETE'})
+    logs = db.nginx.count_documents({})
+    get_count = db.nginx.count_documents({'method': 'GET'})
+    post_count = db.nginx.count_documents({'method': 'POST'})
+    put_count = db.nginx.count_documents({'method': 'PUT'})
+    patch_count = db.nginx.count_documents({'method': 'PATCH'})
+    delete_count = db.nginx.count_documents({'method': 'DELETE'})
 
-print("{} logs".format(logs))
-print("methods:")
-allcounts = {"GET": get_count, "POST": post_count, "PUT": put_count,
-             "PATCH": patch_count, "DELETE": delete_count}
+    print("{} logs".format(logs))
+    print("methods:")
+    allcounts = {"GET": get_count, "POST": post_count, "PUT": put_count,
+                 "PATCH": patch_count, "DELETE": delete_count}
 
-for key, value in allcounts.items():
-    print("\tmethod {}: {}".format(key, value))
+    for key, value in allcounts.items():
+        print("\tmethod {}: {}".format(key, value))
 
-status = db.nginx.count_documents({'method': 'GET', 'path': '/status'})
-print("{} status check".format(status))
+    status = db.nginx.count_documents({'method': 'GET', 'path': '/status'})
+    print("{} status check".format(status))
