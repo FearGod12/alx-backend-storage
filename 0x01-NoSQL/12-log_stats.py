38068d3b09688ee29105bfae7ef14f58d74268be
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-19 05:39:24 -0700

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,20 +7,13 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
 
-    logs = db.nginx.count_documents({})
-    get_count = db.nginx.count_documents({'method': 'GET'})
-    post_count = db.nginx.count_documents({'method': 'POST'})
-    put_count = db.nginx.count_documents({'method': 'PUT'})
-    patch_count = db.nginx.count_documents({'method': 'PATCH'})
-    delete_count = db.nginx.count_documents({'method': 'DELETE'})
+    print("{} logs".format(db.nginx.count_documents({})))
 
-    print("{} logs".format(logs))
-    print("methods:")
-    allcounts = {"GET": get_count, "POST": post_count, "PUT": put_count,
-                 "PATCH": patch_count, "DELETE": delete_count}
-
-    for key, value in allcounts.items():
-        print("\tmethod {}: {}".format(key, value))
-
-    status = db.nginx.count_documents({'method': 'GET', 'path': '/status'})
-    print("{} status check".format(status))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method,
+                                       db.nginx.count_documents
+                                       ({"method": method})))
+    print("{} status check".format(db.nginx.count_documents({
+                                   "method": "GET", "path": "/status"})))
