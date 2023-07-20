b22701878ff8ca1811b89c99ebe2d07597b8dd8f
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-20 21:25:25 +0100

    taks 0

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+<h1> BASICS OF REDIS</h1>
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Class cache"""
+import uuid
+import redis
+
+
+class Cache:
+    """class cache which contains an instance of redis"""
+    def __init__(self):
+        """class constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: any) -> uuid.uuid4():
+        """takes a data argument and returns a string.
+        generates a random key (e.g. using uuid),
+        stores the input data in Redis using the random key and return the key."""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
