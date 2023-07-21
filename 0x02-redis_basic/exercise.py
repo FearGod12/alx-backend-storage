3c9c44f5acbc529440aafe35360517a4fb5fba79
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-21 11:35:37 +0100

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,6 @@ import uuid
 from typing import Union, Callable
 from functools import wraps
 
-
 import redis
 
 
@@ -17,17 +16,36 @@ def count_calls(method: Callable) -> Callable:
         """wrpper fucntion"""
         self._redis.incr(key)
         return method(self, *args, *kwargs)
+
     return increment
+
+
+def call_history(method: Callable) -> Callable:
+    """stores the history of inputs and outputs"""
+    key1 = method.__qualname__ + ":inputs"
+    key2 = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def history(self, *args, **kwargs):
+        """stores the history of inputs and outputs"""
+        self._redis.rpush(key1, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(key2, history(data))
+        return data
+
+    return history
 
 
 class Cache:
     """class cache which contains an instance of redis"""
+
     def __init__(self):
         """class constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[int, float, bytes, str]) -> str:
         """takes a data argument and returns a string.
         generates a random key (e.g. using uuid),
@@ -37,7 +55,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) ->\
+    def get(self, key: str, fn: Callable = None) -> \
             Union[str, float, int, bytes, None]:
         """take a key string argument and an optional Callable argument
         named fn. This callable will be used to convert the data back to
